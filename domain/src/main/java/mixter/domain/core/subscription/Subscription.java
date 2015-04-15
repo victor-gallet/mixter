6ab6bc23d4d9f8c988c9adafded406247c670f34
Jean Helou <jean.helou@gmail.com>
2015-04-15 18:13:04 +0200

    [Core][Subscription][Test OK] Given a subscription, when notifying the follower, then a FolloweeMessageQuacked event is published

--- domain/src/main/java/mixter/domain/core/subscription/Subscription.java
+++ domain/src/main/java/mixter/domain/core/subscription/Subscription.java
@@ -3,6 +3,8 @@ package mixter.domain.core.subscription;
 import mixter.domain.DecisionProjectionBase;
 import mixter.domain.Event;
 import mixter.domain.EventPublisher;
+import mixter.domain.core.message.MessageId;
+import mixter.domain.core.subscription.events.FolloweeMessageQuacked;
 import mixter.domain.core.subscription.events.UserFollowed;
 import mixter.domain.core.subscription.events.UserUnfollowed;
 import mixter.domain.identity.UserId;
@@ -22,6 +24,10 @@ public class Subscription {
 
     public void unfollow(EventPublisher eventPublisher) {
         eventPublisher.publish(new UserUnfollowed(projection.id));
+    }
+
+    public void notifyFollower(MessageId messageId, EventPublisher eventPublisher) {
+        eventPublisher.publish(new FolloweeMessageQuacked(projection.id, messageId));
     }
 
     private class DecisionProjection extends DecisionProjectionBase {
